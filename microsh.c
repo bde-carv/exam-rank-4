c55c4dde8e647b382235ba75fbad84b5e00ebb3c
Ben De carvalho <bde-carv@c1s13d64.42wolfsburg.de>
2022-12-09 15:50:34 +0100

    line 62 fixed

--- microsh.c
+++ microsh.c
@@ -1,97 +1,123 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microsh.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-carv <bde-carv@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 18:52:31 by bde-carv          #+#    #+#             */
+/*   Updated: 2022/12/09 15:49:55 by bde-carv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
-int	ft_strlen(char *s)
+int	ft_strlen(char *str)
 {
-	int i = 0;
-	while (s[i])
+	int	i;
+
+	i = 0;
+	while (str[i])
 		i++;
 	return (i);
 }
 
-void	ft_write(char *s1, char *s2)
+void	ft_write(char *err_msg, char *arg)
 {
-	write(STDERR_FILENO, s1, ft_strlen(s1));
-	write(STDERR_FILENO, s2, ft_strlen(s2));
-	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, err_msg, ft_strlen(err_msg));
+	write(STDERR_FILENO, arg, ft_strlen(arg));
+	write(1, "\n", 1);
 }
 
-void	ft_close(int *fd)
+void ft_close(int *fd)
 {
 	if (*fd != -1)
 		close(*fd);
 	*fd = -1;
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-	if (argc < 2)
+	int	i;
+	int	pid;
+	int	first_cmd;
+	int	fd_in[2] = {-1, -1};
+	int	fd_out[2] = {-1, -1};
+
+	i = 0;
+	pid = 0;
+	first_cmd = 1;
+
+	if (ac < 2)
 		return (0);
-	int i = 0, cmd = 1, pid = 0, fd_in[2] = {-1, -1}, fd_out[2] = {-1, -1};
-	while(argv[i])
+	
+	while (av[i])
 	{
 		i++;
-		while (argv[i] && argv[i][0] == ';')
+		while (av[i] && av[i][0] == ';')
 			i++;
-		if (!argv[i])
+		if (!av[i])
 			break;
-		if (strcmp(argv[i], "cd") == 0)
+		
+		if (strcmp(av[i], "cd") == 0)
 		{
-			cmd = i;
-			while (argv[i] && argv[i][0] != ';')
+			first_cmd = i; // position of cd
+			while (av[i] && av[i][0] != ';') /* check nr of args of cd. must be 2 (= cd + path)*/
 				i++;
-			if (i - cmd != 2)
+			if (i - first_cmd != 2)
 				ft_write("error: cd: bad arguments", "");
-			else if (chdir(argv[cmd + 1]) == -1)
-				ft_write("error: cd: cannot change directory to ", argv[cmd + 1]);
+			else if (chdir(av[first_cmd + 1]) == -1)
+				ft_write("error: cd: cannot change directory to ", av[first_cmd + 1]);
 		}
 		else
 		{
-			cmd = i;
+			first_cmd = i;
 			i++;
-			while (argv[i])
+
+			while (av[i])
 			{
-				if (argv[i][0] == '|')
+				if (av[i][0] == '|') // go through until | or ; is found which marks a cmd to execute
 				{
-					if (pipe(fd_out) == -1)
+					if (pipe (fd_out) == -1) // creating pipe
 						ft_write("error: fatal", "");
 					break;
 				}
-				else if (argv[i][0]  == ';')
+				else if (av[i][0] == ';' && !av[i][1])
 					break;
 				i++;
 			}
 			pid = fork();
-			if (pid == 0)
+			if (pid == 0) // pid= 0 means we are in child, pid != 0 means parent process
 			{
 				if (fd_out[1] != -1)
 				{
 					close(fd_out[0]);
-					dup2(fd_out[1], STDOUT_FILENO);
+					dup2(fd_out[1], STDOUT_FILENO); // stdout(terminal) now refers to fd_out[1] (output of the command);
 					close(fd_out[1]);
 				}
-				if (fd_in[0] != -1)
+				if (fd_in[1] != -1)
 				{
-					close(fd_in[1]);
-					dup2(fd_in[0], STDIN_FILENO);
+					close(fd_in[0]);
+					dup2(fd_out[0], STDIN_FILENO);
 					close(fd_in[0]);
 				}
-				argv[i] = NULL;
-				if (execve(argv[cmd], &argv[cmd], envp) == -1)
-					ft_write("error: cannot execute ", argv[cmd]);
-				exit(0);
+				av[i] = NULL; // temporarily nullterminating the part (=current cmd to execute) for execve. is necessary for execve that the string is 0 temrinated.
+				if (execve(av[first_cmd], &av[first_cmd], envp) == -1)
+					ft_write("error: cannot execute ", av[first_cmd]);
+				exit (0);
 			}
 			else if (pid == -1)
 				ft_write("error: fatal", "");
-			ft_close(&fd_in[0]);
-			ft_close(&fd_in[1]);
+			ft_close(&fd_out[0]);
+			ft_close(&fd_out[1]);
 			fd_in[0] = fd_out[0];
 			fd_in[1] = fd_out[1];
 			fd_out[0] = -1;
 			fd_out[1] = -1;
-			if (pid > 0)
+			if (pid > 0) // parent waiting for child to terminate
 				waitpid(pid, NULL, WUNTRACED);
 		}
 	}
